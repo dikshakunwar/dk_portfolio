d109f446b58a85337e304b803ee991f386dca8b6
Diksha Kunwar <dikshakunwar@Mac.lan>
2025-07-09 23:12:50 +0530

    first-step

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+import { CssSyntaxError } from 'postcss';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  content: [],
+  theme: {
+    colors: {
+      brown: '#A0522D',
+      brownLight: '#D2B48C',
+      brownDark: '#8B4513',
+      black: '#000000',
+      white: '#FFFFFF',
+      cyan: '#00FFFF',
+      cyanLight: '#E0FFFF',
+      cyanDark: '#008B8B',
+      orange: '#FFA500',
+      orangeLight: '#FFDAB9',
+      orangeDark: '#FF8C00',
+      grey: '#808080',
+      greyLight: '#D3D3D3',
+      greyDark: '#696969',
+    },
+    extend: {},
+    fontFamily: {
+      body: ['joselin sans'],
+      special: ['Roboto'],
+    },
+  },
+    plugins: [],
+  };
+
+
