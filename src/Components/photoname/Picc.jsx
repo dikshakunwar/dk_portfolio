f94b9b62a036d2c79e8f5db60b033a908d04e101
Diksha Kunwar <dikshakunwar@Mac.lan>
2025-07-16 15:56:17 +0530

    h

--- src/Components/photoname/Picc.jsx
+++ src/Components/photoname/Picc.jsx
@@ -4,7 +4,7 @@ const Picc = () => {
     <div className="h-full flex item-center justify-center">
       <img
         className=" md:-mt-20 h-[50%] max-h[450px] w-[30%] mx-auto my-0 lg:max-h-full sm:max-h-[800px] sm:w-[70%] sm:mt-0 md:w-[90%] md:max-h-full sm:rounded-full md:rounded-none object-cover"
-        src="../../public/Images/WhatsApp_Image_2025-07-10_at_00.53.04__2_-removebg-preview.png"
+        src="/dk_portfolio/Images/WhatsApp_Image_2025-07-10_at_00.53.04__2_-removebg-preview.png"
         alt=""
       />
     </div>
