850482746fe601c0765cf8a4115b5e6051f1815c
Diksha Kunwar <dikshakunwar@Mac.lan>
2025-07-16 21:57:32 +0530

    done

--- src/Components/footer/FooterMain.jsx
+++ src/Components/footer/FooterMain.jsx
@@ -3,10 +3,10 @@ const FooterMain = () => {
   return (
     <div className="px-4 bg-grey/50 h-[70px] mt-[70px]">
       <div className="flex md:justify-between mt-4 max-w-[1200px] mx-auto  text-black font-body2 ">
-        <p className="text-3xl mt-2 font-body3 uppercase sm:hidden md:block">
+        <p className="text-3xl mt-2 font-body3 uppercase sm:hidden md:block ">
           Diksha Kunwar
         </p>
-        <p className="flex mt-3 sm:ml-[130px]">
+        <p className="flex mt-3 sm:max-w-full sm:mx-auto md:-mr-[5px]">
           © 2025 Diksha Kunwar. All rights reserved.
         </p>
       </div>
