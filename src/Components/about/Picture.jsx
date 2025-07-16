a2fd79a3352a81ab651149161fc330275e6a7fbd
Diksha Kunwar <dikshakunwar@Mac.lan>
2025-07-16 16:03:58 +0530

    last

--- src/Components/about/Picture.jsx
+++ src/Components/about/Picture.jsx
@@ -4,7 +4,7 @@ const Picture = () => {
       <div className="flex justify-center mt-[10%]">
         <img
           className=" max-h-[400px] lg:w-[450px] md:w-[200px] md:max-w-auto  md:max-h-full lg:block md:hidden sm:hidden  ml-[250px] -mt-[5%] "
-          src="../../public/Images/experience-image.png"
+          src="/dk_portfolio/Images/experience-image.png"
           alt=""
         />
       </div>
